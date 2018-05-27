2eccbd7f020c61fe12513a0861a89db708ceb9fb
Komarov Alexandr <KomarovAlea@gmail.com>
2018-05-27 14:24:10 +0300

    Create module for task 7
    Создать snippet (блок что можно выбрать в Билдере) отражающий 6 картинок,
    каждая из которых ведет на другую страницу,
    картинки можно сменять с билдера.

---
+++ task_7/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Task 7',
+    'version': '11.0.1.0',
+    'author': "Oleksandr Komarov",
+    'website': 'https://modool.pro',
+    'summary': '',
+    'description': """
+Создать snippet (блок что можно выбрать в Билдере) отражающий 6 картинок, 
+каждая из которых ведет на другую страницу, картинки можно сменять с билдера.
+    """,
+    'category': 'Website',
+    'depends': ['website'],
+    'data': [
+        'views/snippets.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
